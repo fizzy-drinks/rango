9426c1fef9ca13edb4e25fc313e74b61b28bb63d
Gabriel Chiconi <gabriel@lins.dev>
2022-08-18 19:56:37 -0300

    improve debug route

--- pages/api/wotd.ts
+++ pages/api/wotd.ts
@@ -1,10 +1,20 @@
 import getWotd from '@data/services/getWotd';
 import ApiResponse from '@data/types/ApiResponse';
+import { add, eachDayOfInterval, sub } from 'date-fns';
 import { NextApiHandler } from 'next';
 
-const handler: NextApiHandler<ApiResponse<string>> = (req, res) => {
+const handler: NextApiHandler<ApiResponse<{ date: Date; wotd: string }[]>> = (
+  req,
+  res
+) => {
   if (req.method === 'GET') {
-    return res.send({ success: true, data: getWotd().name });
+    return res.send({
+      success: true,
+      data: eachDayOfInterval({
+        start: sub(new Date(), { days: 30 }),
+        end: add(new Date(), { days: 30 }),
+      }).map((date) => ({ date, wotd: getWotd(date).name })),
+    });
   }
 
   res.statusCode = 405;
