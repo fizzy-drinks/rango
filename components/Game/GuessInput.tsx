5989226369fb29575a7ee78c643e86a9fe229a51
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2022-10-29 14:02:44 -0300

    fix suggestion cursor after a guess

--- components/Game/GuessInput.tsx
+++ components/Game/GuessInput.tsx
@@ -24,6 +24,7 @@ const GuessInput: FC<{
     const guessResult = await sendGuess(guess);
     onChange([...guesses, guessResult]);
     setGuessInputValue('');
+    setActiveSuggestion(0);
   };
 
   const kbdAction = (event: KeyboardEvent<HTMLInputElement>) => {
